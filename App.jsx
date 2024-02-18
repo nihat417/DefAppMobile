f63d2acc50b1b86cb87d4fdd226de826d793fc9f
nihat417 <akreminihat@gmail.com>
2024-02-19 00:06:52 +0400

    First Commit

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomePage from './src/HomePage/HomePage'
+
+const App = () => {
+  return (
+    <HomePage/>
+  )
+}
+
+export default App
---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyledView,StyledText } from '../common/StyledComponents'
+
+const HomePage = () => {
+  return (
+    <StyledView>
+        <StyledText className='bg-red-600 '>
+            salam
+        </StyledText>
+    </StyledView>
+  )
+}
+
+export default HomePage
