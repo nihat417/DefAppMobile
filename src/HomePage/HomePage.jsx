aa350fea8f20821eebc7fb622b24cd28c9498176
nihat417 <akreminihat@gmail.com>
2024-02-19 00:53:39 +0400

    Added svg

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { StyledView,StyledText } from '../common/StyledComponents'
+import React from 'react';
+import {StyledView, StyledText} from '../common/StyledComponents';
+// import Svg, {SvgXml} from 'react-native-svg';
+import Leftarrow from '../assets/svg/LeftArrow';
 
 const HomePage = () => {
   return (
     <StyledView>
-        <StyledText className='bg-red-600 '>
-            salam
-        </StyledText>
+      <Leftarrow width={50} height={50} stroke="black" />
+      <StyledText className="bg-red-600 ">salam</StyledText>
     </StyledView>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/common/StyledComponents.jsx
+++ src/common/StyledComponents.jsx
@@ -1,5 +1,6 @@
 import { Text,View } from "react-native";
 import { styled } from "nativewind";
 
+
 export const StyledView = styled(View);
 export const StyledText = styled(Text);
