76ea7a0c96ca4057cd147eda9063691a195f0669
nihat417 <akreminihat@gmail.com>
2024-02-19 21:17:54 +0400

    Some Updates

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import {StyledView, StyledText} from '../common/StyledComponents';
+import {
+  StyledView,
+  StyledText,
+  Leftarrow,
+  SearchSvg,
+  StyledImage,
+} from '../common/StyledComponents';
 // import Svg, {SvgXml} from 'react-native-svg';
-import Leftarrow from '../assets/svg/LeftArrow';
 
 const HomePage = () => {
   return (
-    <StyledView>
-      <Leftarrow width={50} height={50} stroke="black" />
-      <StyledText className="bg-red-600 ">salam</StyledText>
+    <StyledView style={{backgroundColor: '#Fff'}}>
+      <StyledView className="flex flex-row justify-between mt-[30px] ml-[20px] mr-[10px]">
+        <Leftarrow width={50} height={50} stroke="black" />
+        <StyledText>12 January 2023</StyledText>
+        <SearchSvg width={50} height={50} stroke="green" />
+      </StyledView>
+
+      <StyledView className="h-[30px] bg-red-700"></StyledView>
+
+      <StyledView style={{backgroundColor: '#E4E4E4'}} className="flex">
+        <StyledView className="flex  mt-[30px] ml-[20px] mr-[20px]">
+          <StyledText>Salads</StyledText>
+          <StyledView className="flex flex-row rounded-[20px] mt-[20px]">
+            <StyledImage source={require('../assets/images/salad.png')} />
+            <StyledView style={{backgroundColor: '#fff'}} className="flex">
+              <StyledText>Vegeterian bowl</StyledText>
+              <StyledText>
+                Lorem ipsum dolor sit amet, consectetur adipisci
+              </StyledText>
+              <StyledView className="flex flex-row">
+                <StyledText>4 items left</StyledText>
+                <StyledText>12 AZN</StyledText>
+              </StyledView>
+            </StyledView>
+          </StyledView>
+        </StyledView>
+      </StyledView>
     </StyledView>
   );
 };
--- src/common/StyledComponents.jsx
+++ src/common/StyledComponents.jsx
@@ -1,6 +1,10 @@
-import { Text,View } from "react-native";
+import { Text,View,Image } from "react-native";
 import { styled } from "nativewind";
+import Leftarrow from '../assets/svg/LeftArrow';
+import SearchSvg from "../assets/svg/SearchSvg"
 
 
 export const StyledView = styled(View);
 export const StyledText = styled(Text);
+export const StyledImage = styled(Image);
+export {Leftarrow,SearchSvg}
