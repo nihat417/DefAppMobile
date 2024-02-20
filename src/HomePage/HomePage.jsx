6a2821e2d511d7ba9e089308071bb08946a025f1
nihat417 <akreminihat@gmail.com>
2024-02-20 19:15:30 +0400

    Some Updates

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   StyledView,
+  StyledScrollView,
   StyledText,
   Leftarrow,
   SearchSvg,
@@ -11,32 +12,82 @@ import {
 const HomePage = () => {
   return (
     <StyledView style={{backgroundColor: '#Fff'}}>
+      {/* header */}
       <StyledView className="flex flex-row justify-between mt-[30px] ml-[20px] mr-[10px]">
         <Leftarrow width={50} height={50} stroke="black" />
         <StyledText>12 January 2023</StyledText>
         <SearchSvg width={50} height={50} stroke="green" />
       </StyledView>
 
-      <StyledView className="h-[30px] bg-red-700"></StyledView>
+      {/* nav */}
+      <StyledView className="flex flex-row h-[30px]">
+        <StyledText
+          style={{fontSize: 15}}
+          className="mt-[5px] ml-[20px] mr-[20px]">
+          All
+        </StyledText>
+        <StyledText
+          style={{fontSize: 15}}
+          className="mt-[5px] ml-[20px] mr-[20px]">
+          Desserts
+        </StyledText>
+        <StyledText
+          style={{fontSize: 15}}
+          className="mt-[5px] ml-[20px] mr-[20px]">
+          Salads
+        </StyledText>
+        <StyledText
+          style={{fontSize: 15}}
+          className="mt-[5px] ml-[20px] mr-[20px]">
+          Burgers
+        </StyledText>
+        <StyledText
+          style={{fontSize: 15}}
+          className="mt-[5px] ml-[10px] mr-[10px]">
+          Chicken
+        </StyledText>
+      </StyledView>
 
-      <StyledView style={{backgroundColor: '#E4E4E4'}} className="flex">
-        <StyledView className="flex  mt-[30px] ml-[20px] mr-[20px]">
-          <StyledText>Salads</StyledText>
-          <StyledView className="flex flex-row rounded-[20px] mt-[20px]">
-            <StyledImage source={require('../assets/images/salad.png')} />
-            <StyledView style={{backgroundColor: '#fff'}} className="flex">
-              <StyledText>Vegeterian bowl</StyledText>
-              <StyledText>
+      {/* main */}
+      <StyledScrollView
+        style={{backgroundColor: '#E4E4E4'}}
+        className="px-5 w-full h-full">
+        {/* components */}
+        <StyledView>
+          <StyledText className="mt-[20px]">Salads</StyledText>
+          <StyledView className="flex flex-row mt-[20px]">
+            <StyledImage
+              style={{borderBottomLeftRadius: 10, borderTopLeftRadius: 10}}
+              source={require('../assets/images/salad.png')}
+            />
+            <StyledView
+              style={{
+                backgroundColor: '#fff',
+                borderTopRightRadius: 10,
+                borderBottomRightRadius: 10,
+              }}
+              className="flex flex-col w-[240px]">
+              <StyledView className="flex flex-row justify-between mt-[10px] ">
+                <StyledText className="ml-[20px]">Vegeterian bowl</StyledText>
+                <StyledImage
+                  className="mr-[20px]"
+                  source={require('../assets/images/newImage.png')}
+                />
+              </StyledView>
+              <StyledText className="mt-[20px] mb-[20px] ml-[20px]">
                 Lorem ipsum dolor sit amet, consectetur adipisci
               </StyledText>
-              <StyledView className="flex flex-row">
-                <StyledText>4 items left</StyledText>
-                <StyledText>12 AZN</StyledText>
+              <StyledView className="flex flex-row justify-between">
+                <StyledText className="ml-[20px]">4 items left</StyledText>
+                <StyledText className="mr-[20px]">12 AZN</StyledText>
               </StyledView>
             </StyledView>
           </StyledView>
+
         </StyledView>
-      </StyledView>
+
+        
+      </StyledScrollView>
     </StyledView>
   );
 };
--- src/common/StyledComponents.jsx
+++ src/common/StyledComponents.jsx
@@ -1,4 +1,4 @@
-import { Text,View,Image } from "react-native";
+import { Text,View,Image,ScrollView } from "react-native";
 import { styled } from "nativewind";
 import Leftarrow from '../assets/svg/LeftArrow';
 import SearchSvg from "../assets/svg/SearchSvg"
@@ -6,5 +6,6 @@ import SearchSvg from "../assets/svg/SearchSvg"
 
 export const StyledView = styled(View);
 export const StyledText = styled(Text);
+export const StyledScrollView = styled(ScrollView);
 export const StyledImage = styled(Image);
 export {Leftarrow,SearchSvg}
