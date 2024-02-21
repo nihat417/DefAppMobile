fa2a792592f6c9c0173380c222a24aedb152f43a
nihat417 <akreminihat@gmail.com>
2024-02-21 13:41:18 +0400

    Update Structure

--- src/HomePage/common/HomePage.jsx
+++ src/HomePage/common/HomePage.jsx
@@ -6,13 +6,10 @@ import {
   StyledImage,
   Leftarrow,
   SearchSvg,
-  SubscriptionSvg,
-  HomeSvg,
-  ScanSvg,
-  HamburgerSvg,
 } from '../../common/StyledComponents';
 
 import EmptyMenu from '../components/EmptyMenu';
+import NavTabmenu from '../components/NavTabMenu'
 // import Svg, {SvgXml} from 'react-native-svg';
 
 const HomePage = () => {
@@ -150,7 +147,7 @@ const HomePage = () => {
 
         {/* emptymenu */}
         {/* <EmptyMenu/> */}
-        
+
       </StyledScrollView>
 
       {/* vieworder */}
@@ -188,42 +185,9 @@ const HomePage = () => {
         </StyledView>
       </StyledView>
 
-      {/* navmenu */}
-      <StyledView
-        style={{backgroundColor: 'fff'}}
-        className="flex flex-row justify-between">
-        <StyledView className="mt-[15px] ml-[40px]">
-          <HomeSvg className="ml-[8px]" width={20} height={30} stroke="black" />
-          <StyledText style={{color:'#757575',fontFamily:'Poppins-Light',fontWeight:500}}>Home</StyledText>
-        </StyledView>
-        <StyledView className="mt-[15px] ml-[20px]">
-          <SubscriptionSvg
-            className="ml-[24px]"
-            width={30}
-            height={30}
-            stroke="black"
-          />
-          <StyledText style={{color:'#757575',fontFamily:'Poppins-Light',fontWeight:500}}>Subscription</StyledText>
-        </StyledView>
-        <StyledView className="mt-[15px] ml-[30px]">
-          <ScanSvg
-            className=" ml-[2.5px]"
-            width={30}
-            height={30}
-            stroke="black"
-          />
-          <StyledText style={{color:'#757575',fontFamily:'Poppins-Light',fontWeight:500}}>Scan</StyledText>
-        </StyledView>
-        <StyledView className="mt-[15px] ml-[30px] mr-[30px]">
-          <HamburgerSvg
-            className="ml-[4.5px]"
-            width={30}
-            height={30}
-            stroke="black"
-          />
-          <StyledText style={{color:'#757575',fontFamily:'Poppins-Light',fontWeight:500}}>Menu</StyledText>
-        </StyledView>
-      </StyledView>
+
+      <NavTabmenu/>
+    
     </StyledView>
   );
 };
